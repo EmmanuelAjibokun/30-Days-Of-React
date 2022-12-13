ba6769b69d56c389d03fa5c78d2f5afbebc26eef
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2022-12-13 07:31:30 +0100

    Task 3
    shopping cart add, remove, edit items

--- 01_Day_JavaScript_Refresher/Day 01/exercise-level-2.js
+++ 01_Day_JavaScript_Refresher/Day 01/exercise-level-2.js
@@ -4,3 +4,15 @@ let text =
   'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
 console.log(text.split(/[\W*\s]/))
 console.log(text.length)
+
+
+// In the following shopping cart add, remove, edit items
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
+if (shoppingCart.indexOf("Meat") < 0) {
+  shoppingCart.unshift("Meat")
+}
+if (!shoppingCart.includes("Sugar")) {
+  shoppingCart.push("Sugar")
+}
+shoppingCart[shoppingCart.indexOf("Tea")] = "Green Tea" 
+console.log(shoppingCart)

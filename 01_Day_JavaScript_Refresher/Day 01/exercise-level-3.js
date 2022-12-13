7af109e7a0011b3e0cae4ab5b31dc18832ec226e
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2022-12-13 08:18:44 +0100

    calculated Max, Min and Median

--- 01_Day_JavaScript_Refresher/Day 01/exercise-level-3.js
+++ 01_Day_JavaScript_Refresher/Day 01/exercise-level-3.js
@@ -1,3 +1,10 @@
 const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 ages.sort()
 console.log(ages)
+const min = ages[0]
+const max = ages[-1]
+
+// find the median Age
+const sumOfTwo = ages[ages.length/2] + ages[ages.length/2 - 1]
+const median = sumOfTwo/2
+console.log(median)

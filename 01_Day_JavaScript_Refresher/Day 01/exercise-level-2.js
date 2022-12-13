f8c88fa89e525cd708ebd0bc6770341ce1de2aab
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2022-12-13 07:16:49 +0100

    Exercise for Day1
    Level-1
    Level-2

---
+++ 01_Day_JavaScript_Refresher/Day 01/exercise-level-2.js
@@ -0,0 +1,6 @@
+// Remove all punctuations from the texts
+
+let text =
+  'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+console.log(text.split(/[\W*\s]/))
+console.log(text.length)
---
+++ 01_Day_JavaScript_Refresher/Day 01/main.js
@@ -0,0 +1,3 @@
+// import { countries } from "./countries";
+// console.log(countries[1])
+// console.log("manny")

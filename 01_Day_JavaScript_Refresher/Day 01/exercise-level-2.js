11427591a7d4ae1fb5fa3885b51886e7590e5ac4
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2022-12-14 23:11:47 +0100

    CHECK IF THE SEASON IS AUTUMN, WINTER, SPRING OR SUMMER

--- 01_Day_JavaScript_Refresher/Day 01/exercise-level-2.js
+++ 01_Day_JavaScript_Refresher/Day 01/exercise-level-2.js
@@ -16,3 +16,47 @@ if (!shoppingCart.includes("Sugar")) {
 }
 shoppingCart[shoppingCart.indexOf("Tea")] = "Green Tea" 
 console.log(shoppingCart)
+
+
+// Check if the season is Autumn, Winter, Spring or Summer
+const season = prompt("Which month is this? ").toLowerCase()
+switch(season) {
+  case "september":
+    console.log("The season is Autumn")
+    break
+  case  "october":
+    console.log("The season is Autumn")
+    break
+  case "november":
+    console.log("The season is Autumn")
+    break
+  case "december":
+    console.log("The season is winter")
+    break
+  case "january":
+    console.log("The season is winter")
+    break
+  case "february":
+    console.log("The season is Winter")
+    break
+  case "march":
+    console.log("The season is Spring")
+    break
+  case "april":
+    console.log("The season is Spring")
+    break
+  case "may":
+    console.log("The season is Spring")
+    break
+  case "june":
+    console.log("The season is Summer")
+    break
+  case "july":
+    console.log('The season is Summer')
+    break
+  case "august":
+    console.log("The season is Summer")
+    break
+  default: 
+    console.log(`'${season}' is not a month of the year`)
+}

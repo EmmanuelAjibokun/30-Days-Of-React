3ba931f7e9ba0925f422d9e0882d7bf37082facf
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2022-12-20 21:52:18 +0100

    test window scope

--- 01_Day_JavaScript_Refresher/Day 01/main.js
+++ 01_Day_JavaScript_Refresher/Day 01/main.js
@@ -1,3 +1,15 @@
 // import { countries } from "./countries";
 // console.log(countries[1])
 // console.log("manny")
+
+//scope.js
+
+a = 'JavaScript' // is a window scope this found anywhere
+b = 10 // this is a window scope variable
+function letsLearnScope() {
+  console.log(a, b)
+  if (true) {
+    console.log(a, b)
+  }
+}
+console.log(a, b) // accessible

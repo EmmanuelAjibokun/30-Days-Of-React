0bac061b91ccbe0f7be81acdb4bb5674b9e10759
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2022-12-20 20:23:25 +0100

    conditional statement for number of days in a month
    Switch statement

--- 01_Day_JavaScript_Refresher/Day 01/exercise-level-3.js
+++ 01_Day_JavaScript_Refresher/Day 01/exercise-level-3.js
@@ -21,3 +21,27 @@ console.log(range)
 // min - average
 console.log("Min-Average", Math.abs(min - averageAge))
 console.log("Max-Average", Math.abs(max - averageAge))
+
+
+// Write a program which tells the number of days in a month
+const month = prompt("Enter a month: ").toLowerCase()
+switch(month) {
+  case "september":
+    console.log("September has 30 days.")
+    break
+  case "april":
+    console.log("April has 30 days.")
+    break
+  case "june":
+    console.log("June has 30 days.")
+    break
+  case "november":
+    console.log("November has 30 days.")
+    break
+  case "february":
+    console.log("February has 28 days.")
+    break
+  default: 
+    console.log(`${month} has 31 days`)
+  
+}
